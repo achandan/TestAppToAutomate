107f663046bed0287e4d554d6f8cf5bf4e1d8ff1
achandan <achandan@whitehedge.com>
2016-07-12 18:12:49 +0530

    fisrt commit

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by ArtiC on 7/11/16.
+//  Copyright © 2016 ArtiC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by ArtiC on 7/11/16.
+//  Copyright © 2016 ArtiC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
